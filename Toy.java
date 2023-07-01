05d36d6324c27456f52b5d70530307c3be580a6c
Kailar0000 <kailar0000@gmail.com>
2023-07-01 21:23:34 +0300

    Toy

---
+++ Toy.java
@@ -0,0 +1,15 @@
+public class Toy {
+    int id;
+    String name;
+    double weight;
+
+    public  Toy(int id, String name, double weight){
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public get_id(){
+        return id;
+    }
+}
