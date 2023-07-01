3ebbf9dec6d1c7cb6e5e183d028bb86e128f8876
Kailar0000 <kailar0000@gmail.com>
2023-07-02 01:08:50 +0300

    Test

--- Program.java
+++ Program.java
@@ -1,4 +1,7 @@
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -6,10 +9,17 @@ import java.util.List;
 public class Program {
     public static void main(String[] args) {
 
+        try {
+            PrintWriter pw = new PrintWriter("Toys.txt");
+            pw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         Toy toy1 = new Toy(0, 10, "Кукла", 25);
         Toy toy2 = new Toy(1, 15, "Машинка", 40);
         Toy toy3 = new Toy(2, 13, "Робот", 3);
-        Toy toy4 = new Toy(3, 8, "Матрёшка", 65);
+        Toy toy4 = new Toy(4, 8, "Матрёшка", 65);
 
         List<Toy> toys = new ArrayList<Toy>();
         toys.add(toy4);
--- ToyStore.java
+++ ToyStore.java
@@ -1,6 +1,10 @@
 
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class ToyStore {
@@ -20,16 +24,22 @@ public class ToyStore {
     public void saveToyForLottery() {
         Toy toy = getToyForPrice();
         String text = toy.toString();
-        try(FileWriter writer = new FileWriter("toy-store/ToyStore/Toys.txt", true))
+        text = text + "\n";
+        try( OutputStreamWriter writer = new OutputStreamWriter(
+                new FileOutputStream("Toys.txt", true), 
+                Charset.forName("UTF-8").newEncoder()
+            ))
         { 
             writer.write(text);
-            writer.append('\n');
             writer.flush();
         }
         catch (IOException ex) {
 
             System.out.println(ex.getMessage());
         }
-        toys.remove(toy);
+        if (toy.end_toy() == false){
+            toys.remove(toy);
+        }
+        
     }
 }
