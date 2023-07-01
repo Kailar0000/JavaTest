85d3630a587495b1f7acf617159f70958d5bb5eb
Kailar0000 <kailar0000@gmail.com>
2023-07-01 21:25:05 +0300

    Toy2

--- Toy.java
+++ Toy.java
@@ -1,15 +1,42 @@
-public class Toy {
+public class Toy{
     int id;
+    int num;
     String name;
     double weight;
 
-    public  Toy(int id, String name, double weight){
+    public  Toy(int id, int num, String name, double weight){
         this.id = id;
+        this.num = num;
         this.name = name;
         this.weight = weight;
     }
 
-    public get_id(){
-        return id;
+    public void set_weight(double new_weight){
+        if (this.weight + new_weight > 1){
+            this.weight = this.weight + new_weight;
+        }
+        else{
+            this.weight = 1;
+        }
+
     }
+
+    public Boolean end_toy(){
+        if(num == 0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    public double get_weight(){
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        this.num = this.num - 1;
+        return "Toy [id=" + id + ", name=" + name + "]";
+    }    
 }
